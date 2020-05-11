0c83d1f00ae4d34fb4c9599f2d3dd5c1d3870666
darylnak <darylnak@no-reply.github.com>
2020-05-11 16:39:04 -0700

    Add feature to disable and enable blocked site with button

--- background.js
+++ background.js
@@ -4,6 +4,9 @@
 
 'use strict';
 
+// Allows button to temporarily unblock site
+var disable = true
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
@@ -12,7 +15,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {hostContains: 'evil.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -20,7 +23,22 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+// Analyze URL and check to see if it gets blocked
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { return {cancel: true}; },
-  {urls: ["*://www.evil.com/*"]},
+  function(details) {
+    return {cancel: details.url.indexOf("://www.evil.com/") != -1 && disable};
+  },
+  {urls: ["<all_urls>"]},
   ["blocking"]);
+
+// Get message from content script (popup.js)
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.isEnable == "enable" && disable == true)
+      disable = false;
+    else
+      disable = true
+  });
--- popup.js
+++ popup.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+
+
 let changeColor = document.getElementById('changeColor');
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -11,10 +13,17 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+
+  // send enable message
+  chrome.runtime.sendMessage({isEnable: "enable"}, function(response) {
+    console.log("sent request to enable site");
   });
+
+
+  // let color = element.target.value;
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.executeScript(
+  //       tabs[0].id,
+  //       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  // });
 };
