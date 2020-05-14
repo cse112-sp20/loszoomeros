ae004d77db0ee5d2bce6b5e3b18aefe18265398a
darylnak <darylnak@no-reply.github.com>
2020-05-13 18:30:38 -0700

    Merge into Vue framework, implement a popup with buttons

---
+++ src/popup/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="extension">
+    <h2>Site Blocker</h2>
+
+    <!-- <input v-model="keyword" placeholder="Enter keyword"> -->
+    
+    <p>Site Status: {{ isEnabled }}</p>
+    <p>Extension Status: {{ isOn }}</p>
+
+    <button @click="isEnabled = !isEnabled">Block</button>
+    <button v-on:click="extState">On</button>
+
+    <button @click="count += 1">Count Up</button>
+    <p>The button was clicked {{ count }} times.</p>
+
+    <!-- <p>To get more articles visit <a target="_blank" href="https://www.mynotepaper.com/" style="text-decoration: none;">mynotepaper.com</a></p> -->
+  
+  </div>
+</template>
+
+<script>
+var isEnabled = true;
+var isOn = true;
+var count = 0;
+const browser = require("webextension-polyfill");
+export default {
+  
+  data() {
+    return {
+      keyword: "",
+      isOn: isOn ? "On" : "Off",
+      isEnabled: isEnabled ? "Allowed" : "Blocked",
+      count:count,
+    };
+  },
+  methods: {
+    buttonSearch() {
+      window.open("https://www.mynotepaper.com/?s=" + this.keyword);
+    },
+    siteState() {
+      isOn = !isOn;
+    },
+    extState() {
+      isEnabled = !isEnabled
+    }
+  },
+
+  
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 16px;
+}
+.extension {
+  width: 300px;
+  text-align: center;
+}
+</style>
