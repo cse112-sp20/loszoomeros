f56f5376ee0cede5ba6ff36f9c27ffc2f37b034c
paullarsen <pslarsen@ucsd.edu>
2020-06-04 11:59:54 -0700

    Starter UI for calendar and HTML jsdocs page

---
+++ src/calendar/calComp.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import calComp from './calComp.vue'
+import ToggleButton from 'vue-js-toggle-button'
+import 'bootstrap'; 
+//Bootstrap framework
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//Font awesome
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('icon', Icon)
+Vue.use(ToggleButton)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app2',
+
+  render: h => h(calComp),
+})
